da0068d44987df15d137afe49685f46fd87a8d53
Hasan <mr.k779@outlook.com>
2023-10-09 22:28:02 +0600

    Added initial grammar

---
+++ JLox/src/main/kotlin/Expression.kt
@@ -0,0 +1,7 @@
+internal abstract class Expr {
+    internal class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr()
+    internal class Grouping(val expression: Expr) : Expr()
+    internal class Literal(val value: Any?) : Expr()
+    internal class Unary(val operator: Token, val right: Expr) : Expr()
+    internal class Variable(val name: Token) : Expr()
+}
