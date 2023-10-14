4c28fc6b387bf5046053a8101db5a0fb22d8c674
Hasan <mr.k779@outlook.com>
2023-10-15 03:55:29 +0600

    Update PrettyAST.kt

--- KLox/src/main/kotlin/tools/PrettyAST.kt
+++ KLox/src/main/kotlin/tools/PrettyAST.kt
@@ -2,29 +2,39 @@ package tools
 
 import Expr
 import Token
+import TokenType.MINUS
+import TokenType.STAR
 
 fun main(args: Array<String>) {
-    val expression = Expr.Binary(
-        Expr.Unary(Token(TokenType.MINUS, "-", null, 1), Expr.Literal(123)),
-        Token(TokenType.STAR, "*", null, 1),
-        Expr.Grouping(Expr.Literal(45.67))
-    )
+    val expression =
+        Expr.Binary(
+            Expr.Unary(Token(MINUS, "-", null, 1), Expr.Literal(123)),
+            Token(STAR, "*", null, 1),
+            Expr.Grouping(Expr.Literal(45.67))
+        )
 
-    println(prettify(expression))
+    PrettyAST.print(expression)
 }
 
-// prettify an expression
-private fun prettify(expr: Expr): String = when (expr) {
-    is Expr.Binary -> parenthesize(expr.operator.lexeme, expr.left, expr.right)
-    is Expr.Grouping -> parenthesize("group", expr.expression)
-    is Expr.Literal -> if (expr.value == null) "nil" else expr.value.toString()
-    is Expr.Unary -> parenthesize(expr.operator.lexeme, expr.right)
-    is Expr.Variable -> parenthesize(expr.name.lexeme)
-    else -> ""
-}
+object PrettyAST {
+    fun print(expr: Expr) {
+        println(prettify(expr))
+    }
 
-private fun parenthesize(name: String, vararg expressions: Expr): String {
-    val expressionStrings = expressions.map { expr -> prettify(expr) }
-    val concatenatedExpressions = expressionStrings.reduce { acc, expr -> "$acc $expr" }
-    return "($name $concatenatedExpressions)"
+    // prettify an expression
+    private fun prettify(expr: Expr): String =
+        when (expr) {
+            is Expr.Binary -> parenthesize(expr.operator.lexeme, expr.left, expr.right)
+            is Expr.Grouping -> parenthesize("group", expr.expression)
+            is Expr.Literal -> if (expr.value == null) "nil" else expr.value.toString()
+            is Expr.Unary -> parenthesize(expr.operator.lexeme, expr.right)
+            is Expr.Variable -> parenthesize(expr.name.lexeme)
+            else -> ""
+        }
+
+    private fun parenthesize(name: String, vararg expressions: Expr): String {
+        val expressionStrings = expressions.map { expr -> prettify(expr) }
+        val concatenatedExpressions = expressionStrings.reduce { acc, expr -> "$acc $expr" }
+        return "($name $concatenatedExpressions)"
+    }
 }
