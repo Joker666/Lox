0b0e02379099bc71976e4ea52d6bd7acd158205a
Hasan <mr.k779@outlook.com>
2023-10-15 02:23:05 +0600

    Lox object

---
+++ KLox/src/main/kotlin/Lox.kt
@@ -0,0 +1,51 @@
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+import kotlin.system.exitProcess
+
+object Lox {
+    private var hadError: Boolean = false
+
+    @Throws(IOException::class)
+    fun runPrompt() {
+        val input = InputStreamReader(System.`in`)
+        val reader = BufferedReader(input)
+        while (true) {
+            print("> ")
+            val line = reader.readLine() ?: break
+            run(line)
+            hadError = false
+        }
+    }
+
+    @Throws(IOException::class)
+    fun runFile(path: String) {
+        val bytes = Files.readAllBytes(Paths.get(path))
+        run(String(bytes, Charset.defaultCharset()))
+
+        // Indicate an error in the exit code.
+        if (hadError) exitProcess(65)
+    }
+
+    private fun run(source: String) {
+        val scanner = Scanner(source)
+        val tokens = scanner.scanTokens()
+
+        for (token in tokens) {
+            println(token)
+        }
+    }
+
+    fun error(line: Int, message: String) {
+        report(line, "", message)
+    }
+
+    private fun report(line: Int, where: String, message: String) {
+        System.err.println("[line $line] Error$where: $message")
+        hadError = true
+    }
+}
--- KLox/src/main/kotlin/Main.kt
+++ KLox/src/main/kotlin/Main.kt
@@ -1,60 +1,12 @@
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.*
 import kotlin.system.exitProcess
 
-var hadError: Boolean = false
-
 fun main(args: Array<String>) {
-    if (args.size > 1) {
-        println("Usage: klox [script]")
-        exitProcess(64)
-    } else if (args.size == 1) {
-        runFile(args[0])
-    } else {
-        runPrompt()
+    when (args.size) {
+        0 -> Lox.runPrompt()
+        1 -> Lox.runFile(args[0])
+        else -> {
+            println("Usage: klox [script]")
+            exitProcess(64)
+        }
     }
-}
-
-@Throws(IOException::class)
-private fun runFile(path: String) {
-    val bytes = Files.readAllBytes(Paths.get(path))
-    run(String(bytes, Charset.defaultCharset()))
-
-    // Indicate an error in the exit code.
-    if (hadError) exitProcess(65)
-}
-
-@Throws(IOException::class)
-fun runPrompt() {
-    val input = InputStreamReader(System.`in`)
-    val reader = BufferedReader(input)
-    while (true) {
-        print("> ")
-        val line = reader.readLine() ?: break
-        run(line)
-        hadError = false
-    }
-}
-
-fun run(source: String) {
-    val scanner = Scanner(source)
-    val tokens = scanner.scanTokens()
-
-    for (token in tokens) {
-        println(token)
-    }
-}
-
-fun error(line: Int, message: String) {
-    report(line, "", message)
-}
-
-private fun report(line: Int, where: String, message: String) {
-    System.err.println("[line $line] Error$where: $message")
-    hadError = true
 }
