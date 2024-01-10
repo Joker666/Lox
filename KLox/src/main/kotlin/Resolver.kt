b237c7df38e6d3382eb240b01895d08d38f7d74f
Hasan <mr.k779@outlook.com>
2024-01-10 22:48:33 +0600

    Update Resolver.kt

--- KLox/src/main/kotlin/Resolver.kt
+++ KLox/src/main/kotlin/Resolver.kt
@@ -1,5 +1,39 @@
 import java.util.*
 
 class Resolver(private val interpreter: Interpreter) {
-    private val scopes = Stack<Map<String, Boolean>>()
+    private val scopes = Stack<MutableMap<String, Boolean>>()
+
+    fun resolve(statements: List<Stmt>) {
+        statements.forEach { resolve(it) }
+    }
+
+    private fun resolve(stmt: Stmt) {
+        when (stmt) {}
+    }
+
+    private fun resolve(expr: Expr) {
+        when (expr) {}
+    }
+
+    private fun beginScope() {
+        scopes.push(HashMap())
+    }
+
+    private fun endScope() {
+        scopes.pop()
+    }
+
+    private fun declare(name: Token) {
+        if (scopes.isEmpty()) return
+
+        val scope = scopes.peek()
+        scope[name.lexeme] = false
+    }
+
+    private fun define(name: Token) {
+        if (scopes.isEmpty()) return
+
+        val scope = scopes.peek()
+        scope[name.lexeme] = true
+    }
 }
