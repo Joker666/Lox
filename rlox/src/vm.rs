34b921fa7f595acbf222679658d304b60c6b19e6
Rafi <mr.k779@outlook.com>
2024-03-10 05:29:57 +0600

    Update vm.rs

--- rlox/src/vm.rs
+++ rlox/src/vm.rs
@@ -21,7 +21,7 @@ impl VM {
         }
     }
 
-    pub fn interpret(&mut self, source: &String) -> InterpretResult {
+    pub fn interpret(&mut self, source: &str) -> InterpretResult {
         let compiler = Compiler::new();
         compiler.compile(source);
         InterpretResult::Ok
